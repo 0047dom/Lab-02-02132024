34b31569d859ccbdd50e98aeb33803f648119323
0047dom <dommags@gmail.com>
2024-02-27 15:16:29 -0500

    Lab 3 updates
    Updated files and game description md

--- menu.gd
+++ menu.gd
@@ -5,8 +5,11 @@ extends Control
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://waiting_room.tscn")
 	
+
+func _on_guide_pressed():
+	get_tree().change_scene_to_file("res://guide.tscn")
 	
-
-
 func _on_quit_pressed():
-	get_tree().quit()
+	get_tree().quit() 
+
+
